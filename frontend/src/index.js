831c8782815a77d355a214d281ccbbcc7cece98c
AdaN6 <a_disney_da@hotmail.com>
2023-01-11 20:27:19 +0100

    implemented and wrapped the AuthContextProvider in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from "./context/WorkoutsContext";
+import { AuthContextProvider } from "./context/WorkoutsContext";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
