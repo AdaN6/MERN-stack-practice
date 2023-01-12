bc29cda18168e64f03c8918522fb758ff0862ff5
AdaN6 <a_disney_da@hotmail.com>
2023-01-12 14:39:25 +0100

    created a signup hook und updated signup page by using useSignup hook

--- frontend/src/pages/Singup.js
+++ frontend/src/pages/Singup.js
@@ -1,35 +1,40 @@
 import {useState} from "react"
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('');
-
+    const {signup, isLoading, error} = useSignup();
+ 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        // console.log(email, password)
+
+        await signup(email, password)
     }
 
-    return(
-        <form className="signup" onSubmit={handleSubmit}>
-            <h3>Sign up</h3>
-            <lable>Email:</lable>
-            <input 
-            type="email"
-            onChange={(e)=> setEmail(e.target.value)}
-            value={email}>
-            </input>
+    return (
+      <form className="signup" onSubmit={handleSubmit}>
+        <h3>Sign up</h3>
+        <lable>Email:</lable>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></input>
 
-            <lable>Password:</lable>
-            <input 
-            type="password"
-            onChange={(e)=> setPassword(e.target.value)}
-            value={password}>
-            </input>
+        <lable>Password:</lable>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></input>
 
-            <button>Sign up</button>
-        </form>
-    )
+        <button disabled={isLoading}>Sign up</button>
+        {error && <div className="error">{error}</div>}
+      </form>
+    );
 }
 
 export default Signup
