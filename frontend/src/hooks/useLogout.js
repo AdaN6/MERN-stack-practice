da198732fd1d035621757150a6e6c096f7d890c1
AdaN6 <a_disney_da@hotmail.com>
2023-01-12 15:33:02 +0100

    created useLogout hook

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,20 @@
+import { useAuthContext } from "./useAuthContext"
+
+export const useLogout = () => {
+
+    const {dispatch} = useAuthContext()
+
+    const logout = () => {
+        // remove user from storage
+        localStorage.removeItem('user')
+
+        //dispatch lout action
+        dispatch({type: 'LOGOUT'})
+
+    }
+
+    return{logout}
+
+
+        
+}
